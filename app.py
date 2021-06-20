d3fe3ab8941808ed84690a715de6c4fc2514dabf
kang980220 <guslsl123@naver.com>
2021-06-20 19:57:33 +0900

    커밋

--- app.py
+++ app.py
@@ -26,12 +26,21 @@ def join():
     if request.method == "GET":
         return render_template('join.html')
     else:
-        name = request.form['username']
+        name = request.form['name']
         userid = request.form['userid']
-        pwd = request.form['pwd']
+        pw = request.form['pw']
         # 회원정보를 데이터베이스에 넣기
-        dbdb.insert_user(userid, name, pwd)
-        return '<b>{}, {}, {}</b> 님 회원가입 되었습니다.'.format(name, userid, pwd)
+        dbdb.insert_user(userid, name, pw)
+        #ret = dbdb.check_id(id)
+        #if ret != None:
+        #    return'''
+        #            <script>
+        #            alert('다른 아이디를 사용 하세요);
+        #            locatiob.href='/join';
+        #            </script>
+        #          '''
+
+        return redirect(url_for('login'))
 
 # 로그인
 @app.route('/login', methods=['GET', 'POST'])
@@ -40,7 +49,7 @@ def login():
         return render_template('login.html')
     else:
         id = request.form['userid']
-        pw = request.form['pwd']
+        pw = request.form['pw']
         # id와 pw가 임의로 정한 값이랑 비교 해서 맞으면 맞다 틀리면 틀리다
         if id == 'abc' and pw == '1234':
             session['user'] = id
@@ -58,23 +67,6 @@ def logout():
     session.pop('user', None)
     return redirect(url_for('form'))
 
-
-# 로그인 사용자만 접근 가능으로 만들면
-@app.route('/form')
-def form():
-    if 'user' in session:
-        return render_template('test.html')
-    return redirect(url_for('login'))
-
-@app.route('/method', methods=['GET', 'POST'])
-def method():
-    if request.method == 'GET':
-        return 'GET 으로 전송이다.'
-    else:
-        num = request.form["num"]
-        name = request.form["name"]
-        return 'POST 이다. 학번은: {} 이름은: {}'.format(num, name)
-
 @app.route('/search')
 def search():
     # 만약에 로그인 상태이면 검색 페이지 나오고
@@ -83,16 +75,6 @@ def search():
     # 아니면 로그인 페이지로 이동
     else:
         return redirect("main.html")
-
-
-
-@app.route('/action', methods=['GET', 'POST'])
-def action():
-    if request.method == "GET":
-        return '그냥 넘어옴(GET)'
-    else:
-        name = request.form['fname']
-        return '<b>{}</b> 로 검색한 결과입니다. 리스트 쫙~~(POST)'.format(name)
 
 # 축구페이지
 @app.route('/football')
--- dbdb.py
+++ dbdb.py
@@ -2,54 +2,27 @@ import pymysql
 
 def dbcon():
     return pymysql.connect(host='rkdgusdls.mysql.pythonanywhere-services.com',
-                   user='rkdgusdls', password='a123456789',
-                   db='rkdgusdls$mydb', charset='utf8')
-
-def create_table():
+                   user='rkdgusdls', password='h123456789',
+                   db='rkdgusdls$mydb', charset='utf8', port=3307)
+def insert_user(userid, pw, name):
     try:
         db = dbcon()
         c = db.cursor()
-        c.execute("CREATE TABLE student (num varchar(50), name varchar(50))")
+        setdata = (userid, pw, name)
+        c.execute("INSERT INTO user VALUES (%s, %s, %s)", setdata)
         db.commit()
     except Exception as e:
         print('db error:', e)
     finally:
         db.close()
 
-def insert_data(num, name):
-    try:
-        db = dbcon()
-        c = db.cursor()
-        setdata = (num, name)
-        c.execute("INSERT INTO student VALUES (%s, %s)", setdata)
-        db.commit()
-    except Exception as e:
-        print('db error:', e)
-    finally:
-        db.close()
-
-def select_all():
-    ret = list()
-    try:
-        db = dbcon()
-        c = db.cursor()
-        c.execute('SELECT * FROM student')
-        ret = c.fetchall()
-        # for row in c.execute('SELECT * FROM student'):
-        #     ret.append(row)
-    except Exception as e:
-        print('db error:', e)
-    finally:
-        db.close()
-        return ret
-
-def select_num(num):
+def select_user(userid, pw):
     ret = ()
     try:
         db = dbcon()
         c = db.cursor()
-        setdata = (num,)
-        c.execute('SELECT * FROM student WHERE num = %s', setdata)
+        setdata = (userid, pw)
+        c.execute('SELECT * FROM user WHERE userid = %s AND pw = %s', setdata)
         ret = c.fetchone()
     except Exception as e:
         print('db error:', e)
@@ -57,7 +30,7 @@ def select_num(num):
         db.close()
         return ret
 
-create_table()
-insert_data('20201234', '파이썬')
-ret = select_all()
-print(ret)
+#create_table()
+#insert_data('20201234', '파이썬')
+#ret = select_all()
+#print(ret)
